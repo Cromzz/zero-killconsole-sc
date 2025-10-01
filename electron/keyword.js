f971bd45ae6542de0f181f70a5fe087e337b8e05
Cromz <danord309@gmail.com>
2025-10-01 19:08:44 +0100

    added more keywords for better kill display for ship weapons

--- electron/keyword.js
+++ electron/keyword.js
@@ -9,7 +9,7 @@ const terms = {
     'AIModule_Unmanned_PU_SecurityNetwork_': 'Station Security',
     'ksar_shotgun_ballistic_01': 'Ravager Shotgun',
     'behr_lmg_ballistic_01' : 'FS 9 LMG',
-    'ksar_rifle_energy' : 'Karna Rifle red',
+    'ksar_rifle_energy' : 'Karna Rifle',
     'gmni_smg_ballistic_01' : 'C54 SMG',
     'ksar_shotgun_ballistic_01' : 'Ravager Twinshot',
     'behr_shotgun_ballistic_01' : 'BR 2 Shotgun',
@@ -26,6 +26,10 @@ const terms = {
     'klwe_lmg_energy_01_purple_blue01' : 'Demaco Energy LMG',
     'gmni_lmg_ballistic_01' : 'F-55 LMG',
     'utfl_melee_01_red01_gungame ' : 'Red Knife',
+    'KLWE_LaserRepeater_S1' : 'Pather Repeater (S1)',
+    'KLWE_LaserRepeater_S2' : 'Badger Repeater (S2)',
+    'KLWE_LaserRepeater_S3' : 'Pather Repeater (S3)',
+    
     
     // Add more as needed
   };
